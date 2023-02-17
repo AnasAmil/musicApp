fd52dbca00368c07248f6ec6c62f300b116911d4
AnasAmil <anasamaneellah1@gmail.com>
2023-02-17 22:27:56 +0100

    added CRUD for songs

--- musicApp_backend/musicApp_backend/urls.py
+++ musicApp_backend/musicApp_backend/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('songs/', include('songs.urls')), 
+    path('api/songs/', include('songs.urls')), 
     # # for accounts/signup
-    path("accounts/", include("accounts.urls")),
-    path("categories/", include("categories.urls")),
-    path("playlists/", include("playlists.urls")),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/categories/", include("categories.urls")),
+    path("api/playlists/", include("playlists.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- musicApp_backend/songs/urls.py
+++ musicApp_backend/songs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.getSongs),
+    path('<str:pk>', views.getSong),
 ]
