43ad3eca1986be17c7fcdec801f61cb23deb1fbf
AnasAmil <anasamaneellah1@gmail.com>
2023-02-19 02:33:16 +0100

    added nested serializers to songs,playlists

--- musicApp_backend/playlists/serializers.py
+++ musicApp_backend/playlists/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Playlist
+from songs.serializers import SongSerializer
 
 class PlaylistSerializer(ModelSerializer):
+    songs = SongSerializer(many=True, read_only=True)
     class Meta:
         model = Playlist
         fields = '__all__'
