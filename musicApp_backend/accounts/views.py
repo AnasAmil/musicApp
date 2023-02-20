5fee38f45375b40fafc5922135dc2cef13dcdf7d
Mouh-Cine-coder <mouhcinedafiaa@gmail.com>
2023-02-20 02:26:39 +0100

    add getCategorie and CRUD for user

--- musicApp_backend/accounts/views.py
+++ musicApp_backend/accounts/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+from rest_framework import status
 # Create your views here.
 
-def showUser(request):
-    return HttpResponse("user")
+
+@api_view(['GET', 'POST'])
+def showUsers(request):
     
+    if request.method ==  'GET':
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
     
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def showUser(request, pk):
+    
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_201_CREATED)
+        
+        
+        
+            
+        
--- musicApp_backend/categories/views.py
+++ musicApp_backend/categories/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Categories
 from .serializers import CategoriesSerializer
+from rest_framework import status
 # Create your views here.
 
 
@@ -11,3 +12,28 @@ def getCategories(request):
     serializer = CategoriesSerializer(categories, many=True)
 
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def showCategorie(request, pk):
+    
+    try:
+        user = Categories.objects.get(pk=pk)
+    except Categories.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = CategoriesSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = CategoriesSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_201_CREATED)
+        
