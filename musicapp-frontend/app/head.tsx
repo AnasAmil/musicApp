80e9d1d63e0e924d894d6b67a6f66876e3a8126e
AnasAmil <anasamaneellah1@gmail.com>
2023-02-23 14:18:30 +0100

    worked on header

--- musicapp-frontend/app/head.tsx
+++ musicapp-frontend/app/head.tsx
@@ -4,7 +4,6 @@ export default function Head() {
       <title>MusicApp</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="MusicApp" />
-      <link rel="icon" href="/favicon.ico" />
     </>
   )
 }
