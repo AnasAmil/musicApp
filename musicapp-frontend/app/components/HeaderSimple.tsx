d61b7fd2bc33c0d58084cb99ec52a320a8207bf5
AnasAmil <anasamaneellah1@gmail.com>
2023-02-24 13:40:38 +0100

    working on profilePic

--- musicapp-frontend/app/components/HeaderSimple.tsx
+++ musicapp-frontend/app/components/HeaderSimple.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react'
+<<<<<<< Updated upstream
 import { Header, TextInput } from '@mantine/core'
 import { IconSearch } from '@tabler/icons-react';
 
@@ -11,6 +12,18 @@ const HeaderSimple = () => {
         autoComplete='nope'
         icon={<IconSearch size={18} stroke={1.5} color='#F26979' className='rounded-lg' />}
       />
+=======
+import { Header } from '@mantine/core'
+import "../globals.css"
+import SearchInput from './SearchInput'
+import ProfilePic from './ProfilePic'
+
+const HeaderSimple = () => {
+  return (
+    <Header height={60} mb={120} className=' flex items-center border-b-2 bg-transparent w-full border-[#E6D3CF]'>
+      <SearchInput />
+      <ProfilePic />
+>>>>>>> Stashed changes
     </Header>
   )
 }
