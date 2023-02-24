411a2ab7cd3789791d42ed416d730927a49862da
Mouh-Cine-coder <mouhcinedafiaa@gmail.com>
2023-02-24 20:34:41 +0100

    commit changers in header

--- musicapp-frontend/app/components/HeaderSimple.tsx
+++ musicapp-frontend/app/components/HeaderSimple.tsx
@@ -1,18 +1,5 @@
 "use client"
 import React from 'react'
-<<<<<<< Updated upstream
-import { Header, TextInput } from '@mantine/core'
-import { IconSearch } from '@tabler/icons-react';
-
-const HeaderSimple = () => {
-  return (
-    <Header height={60} mb={120}  className=' flex items-center border-b-2 bg-transparent w-full border-[#E6D3CF]'>
-      <TextInput 
-        placeholder='Search'
-        autoComplete='nope'
-        icon={<IconSearch size={18} stroke={1.5} color='#F26979' className='rounded-lg' />}
-      />
-=======
 import { Header } from '@mantine/core'
 import "../globals.css"
 import SearchInput from './SearchInput'
@@ -23,7 +10,6 @@ const HeaderSimple = () => {
     <Header height={60} mb={120} className=' flex items-center border-b-2 bg-transparent w-full border-[#E6D3CF]'>
       <SearchInput />
       <ProfilePic />
->>>>>>> Stashed changes
     </Header>
   )
 }
