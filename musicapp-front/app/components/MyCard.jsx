fc97c74bbe1d959ed2be9e2c901a458cf71011ec
AnasAmil <anasamaneellah1@gmail.com>
2023-03-02 14:43:54 +0100

    fixed color bug

--- musicapp-front/app/components/MyCard.jsx
+++ musicapp-front/app/components/MyCard.jsx
@@ -1,14 +1,20 @@
 'use client'
 import { Image, Text, Paper } from '@mantine/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../globals.css"
 
 
 const MyCard = ({title,color,categorieImage}) => {
 
+    const [inColor, setIncolor] = useState('')
+    
+    useEffect(() => {
+      setIncolor(color)
+    }, [])
+    
 
     return (
-        <Paper  p="lg" className={`bg-[${color}] rounded-lg p-1 shadow-md h-32 overflow-hidden`}>
+        <Paper  p="lg" className={`bg-[${inColor}] rounded-lg p-1 shadow-md h-32 overflow-hidden`}>
             <Text className='font-bold text-white text-lg'>{title}</Text>
             <Image 
                 src={categorieImage}
