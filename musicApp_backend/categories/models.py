2e97b86a185435e7526f6f92c5ee2ebe90b24042
Mouh-Cine-coder <mouhcinedafiaa@gmail.com>
2023-03-06 01:23:48 +0100

    add Ids to all models and remove # from color

--- musicApp_backend/categories/models.py
+++ musicApp_backend/categories/models.py
@@ -3,18 +3,17 @@ from .randomHexColor import random_hex_color
 import uuid
 # Create your models here.
 
+
 class Categories(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200, blank=False, null=False)
     color = models.CharField(max_length=7, blank=False, null=False)
-    categorieImage = models.ImageField(upload_to ='static/categorieImages')
-    
-    
-    def save(self, *args, **kwargs):  
-            if not len(self.color):
-                self.color = random_hex_color()
-            super(Categories, self).save(*args, **kwargs) 
-    
+    categorieImage = models.ImageField(upload_to='static/categorieImages')
+
+    def save(self, *args, **kwargs):
+        if not len(self.color):
+            self.color = random_hex_color()
+        super(Categories, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
-    
